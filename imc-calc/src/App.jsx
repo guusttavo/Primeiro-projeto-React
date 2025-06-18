903b0b0dade3af067f7174ce2f72feb45b302666
guusttavo <gustavoribeiroda112@gmail.com>
2025-06-17 23:06:00 -0300

    Finalizando o primeiro projeto em react

--- imc-calc/src/App.jsx
+++ imc-calc/src/App.jsx
@@ -1,12 +1,35 @@
+import { data } from './components/data/data'
+
+import { useState } from 'react'
+
+import ImcCalc from './components/ImcCalc'
+import ImcTable from './components/ImcTable'
+
 import './App.css'
 
+
 function App() {
- 
+  const [imc, setImc] = useState("");
+  const [info, setInfo] = useState("");
+  const [infoClass, setInfoClass] = useState("");
+
+  const calcImc = (e, height, weight) => {
+    e.preventDefault();
+
+    if (!weight || !height) return;
+
+    const weightFloat = +weight.replace(",", ".");
+    const heightFloat = +height.replace(",", ".");
+
+    const imcResult = (weightFloat / (heightFloat * heightFloat)).toFixed(1);
+
+    setImc(imcResult);
+  }
 
   return (
     <>
-     <div className="App">
-        <p>IMC Calc</p>
+     <div className="container ">
+        {! imc ? <ImcCalc calcImc={calcImc} /> : <ImcTable data={data} />}
      </div>
     </>
   )
